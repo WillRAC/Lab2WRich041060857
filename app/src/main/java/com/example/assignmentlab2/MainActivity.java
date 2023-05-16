993c19a0e0d8663cc57cdc92fe6c920afa072fd5
WillR <willrich@live.co.uk>
2023-05-16 13:46:09 +0300

    Code 1

--- app/src/main/java/com/example/assignmentlab2/MainActivity.java
+++ app/src/main/java/com/example/assignmentlab2/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.assignmentlab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +16,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button PMe = findViewById(R.id.PressMe);
+        EditText txt =  (EditText) findViewById(R.id.editText);
+        String edTxts = txt.getText().toString();
 
-        PMe.setOnClickListener();
+
+        TextView textv = (TextView)findViewById(R.id.textView1);
+
+        Button pMe = findViewById(R.id.PressMe);
+
+        pMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textv.setText(edTxts);
+
+                Toast.makeText(getApplicationContext(), "Label has been Updated", Toast.LENGTH_LONG).show();
+
+
+
+
+            }
+        });
+
 
 
     }
