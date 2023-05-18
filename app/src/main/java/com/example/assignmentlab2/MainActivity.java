3f0dad6527e9bb8abdb4ee91ff9e50e9405816bf
WillR <willrich@live.co.uk>
2023-05-18 12:48:51 +0300

    Final? Maybe?

--- app/src/main/java/com/example/assignmentlab2/MainActivity.java
+++ app/src/main/java/com/example/assignmentlab2/MainActivity.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView texts;
+    String toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,28 +25,48 @@ public class MainActivity extends AppCompatActivity {
         );
 
         EditText txt =  (EditText) findViewById(R.id.editText);
-        String edTexts = txt.getText().toString();
 
-
-        TextView texts = (TextView)findViewById(R.id.textView1);
+        texts = (TextView)findViewById(R.id.textView1);
+        toast = getResources().getString(R.string.Toast_M);
 
         Button pMe = findViewById(R.id.PressMe);
+
+        CheckBox cb = (CheckBox)findViewById(R.id.checkBox);
+
 
         pMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+
+                String edTexts = txt.getText().toString();
+
                 texts.setText(edTexts);
 
-                Toast.makeText(getApplicationContext(), "Label has been Updated", Toast.LENGTH_LONG).show();
-
-
-
-
+                Toast.makeText(MainActivity.this, toast, Toast.LENGTH_LONG).show();
             }
         });
 
+        cb.setOnCheckedChangeListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cb.setChecked(true);
+                cb.setOnCheckedChangeListener( (cb, isChecked) -> {});
+                cb.onCheckChanged(CompoundButton cb, boolean b) {
+                    Snackbar.make(v, "Checkboc is" +  b, Snackbar.LENGTH_LONG).show();
+                    Snackbar.setAction( "undo", click ->cb.setChecked(!b));
+            }}
+        });
 
 
     }
+
+
 }
+
+
+
+
+
+
